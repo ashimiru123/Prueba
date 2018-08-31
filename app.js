5bbc26682266b537896e4480b8cc3d91e07412a2
jeison.gomez <jeison.gomez@pragma.com.co>
2018-08-31 14:28:13 -0500

    formulario

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 var inicio= document.getElementById('iniciar');
 
+var guardar= document.getElementById('guardar');
 
 inicio.addEventListener('click', function iniciar(){
     firebase.auth()
@@ -15,7 +16,31 @@ inicio.addEventListener('click', function iniciar(){
         var t = document.createTextNode(result.user.displayName);
         nombre.appendChild(t);
         imagen.appendChild(img);
+        var datos={
+            nombre:result.user.displayName
+        }
+
+        var variable=firebase.database();
+        variable.setData("usuarios/",datos);
     });
 
 });
 
+guardar.addEventListener('click', function insertar_valor() {
+    var id = document.getElementById('id').value;
+    var Nombre = document.getElementById('Nombre').value;
+    var Apellido = document.getElementById('Apellido').value;
+    var Telefono = document.getElementById('Telefono').value;
+    var Dirección = document.getElementById('Dirección').value;
+    var Correo = document.getElementById('Correo').value;
+    alert(Nombre+Apellido+Telefono+Dirección+Correo)
+    var dataToImport = {
+        id:id,
+        Nombre: Nombre,
+        Apellido: Apellido,
+        Telefono: Telefono,
+        Dirección: Dirección,
+        Correo: Correo
+    };
+    firebase.database().ref("Profesores/"+id).set(dataToImport);
+});
